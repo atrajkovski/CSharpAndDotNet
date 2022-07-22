616c05b79f18244ab1593d3b49e795f7234297bd
atrajkovski <dsandra86@yahoo.com>
2022-07-22 17:56:40 +0200

    finished work on Chapter03

---
+++ Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+int a = 10;
+double b = a; // an int can be safely cast into a double
+WriteLine("{0} type is {1}", b, b.GetType());
+
+double c = 9.8;
+int d =(int) c; // compiler gives an error for this line
+WriteLine("{0} type is {1}", d, d.GetType());
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:NO} and f is {f:NO}");
+
+e = long.MaxValue;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+
+double y = 9.8;
+int i = (int)g;
+WriteLine($"y is {y} and i is {i}");
+
+
+double[] doubles = new[]
+{ 9.49, 9.5, 9.51, 10.49, 10.5, 10.51 };
+foreach (double n in doubles)
+{
+    WriteLine($"ToInt32({n}) is {ToInt32(n)}");
+}
+
+foreach (double n in doubles)
+{
+    WriteLine(format:
+    "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+    arg0: n,
+    arg1: Math.Round(value: n, digits: 0,
+    mode: MidpointRounding.AwayFromZero));
+}
--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -51,3 +51,53 @@ WriteLine("After end of switch");
 A_label:
 WriteLine($"After A_label");
 
+
+string path =  @"C:\Users\Trajkovski\Documents\Full stack Development\CSharpAndDotNet";
+
+Write("Press R for read - only or W for writeable");
+ConsoleKeyInfo key = ReadKey();
+
+Stream? s;
+if (key.Key == ConsoleKey.R)
+{
+    s = File.Open(Path.Combine(path, "file.txt"), FileMode.OpenOrCreate, FileAccess.Read);
+}
+else
+{
+    s = File.Open(Path.Combine(path, "file.txt"), FileMode.OpenOrCreate, FileAccess.Write);
+}
+string message;
+
+//switch (s)
+//{
+//    case FileStream writeableFile when s.CanWrite:
+//        message = "The sream is a file that I can write to.";
+//        break;
+//    case FileStream readOnlyFile:
+//        message = "The stream is a read-only file.";
+//        break;
+//    case MemoryStream ms:
+//        message = "The stream is a memory address.";
+//        break;
+//    default: // always evalueted last despite its current position
+//        message = "The stream is some other type.";
+//        break;
+//    case null:
+//        message = "The stream is null.";
+//        break;
+//}
+
+message = s switch
+{
+    FileStream writeableFile when s.CanWrite => "The sream is a file that I can write to.",
+    FileStream readOnlyFile => "The stream is a read-only file.",
+    MemoryStream ms => "The stream is a memory address.",
+    null => "The stream is null.",
+    _ => "The stream is some other type."
+};
+WriteLine(message);
+
+
+
+
+
